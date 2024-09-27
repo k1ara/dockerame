2c1e0a040a02f241cad1c5168c7b64f1e775f781
Kiara Arosemena <lynmaru@gmail.com>
2024-09-27 16:17:23 -0500

    cpanm en una sola linea

--- ocs-inventory/script-dependencias.sh
+++ ocs-inventory/script-dependencias.sh
@@ -2,6 +2,4 @@ apt update && apt upgrade -y
 
 apt-get install -y cpanminus libxml-entities-perl build-essential libsnmp-dev iputils-ping vim net-tools
 
-cpanm XML::Entities
-
-cpanm SNMP
+cpanm XML::Entities SNMP
