98ac9df93319af552b02cf2dfbaa0215d6a2ac28
Kiara Arosemena <lynmaru@gmail.com>
2024-10-01 11:31:56 -0500

    archivo con modificaciones

--- ocs-inventory/mariadb-instalacion.sh
+++ ocs-inventory/mariadb-instalacion.sh
@@ -6,7 +6,21 @@ docker pull mariadb:10.4
 
 docker images
 
-docker run --name mariadb -e MYSQL_ROOT_PASSWORD=mypass -p 3306:3306 -d docker.io/library/mariadb:10.3
+docker run --name mariadb --network [mi-red] -e MYSQL_ROOT_PASSWORD=mypass -p 3306:3306 -d docker.io/library/mariadb:10.4
+
+#conectar a la misma red que las maquinas que usaran la base de datos
+docker network ls
+docker network connect [mi-red] mariadb
+
+docker exec -it mariadb -u root -p
+
+CREATE USER 'ocs'@'%' IDENTIFIED BY 'ocs24cfe';  #El símbolo % permite que este usuario se conecte desde cualquier dirección IP.
+
+GRANT ALL PRIVILEGES ON ocsweb.* TO 'ocs'@'%';  #se otorgan permisos para la base de datos especificada
+
+FLUSH PRIVILEGES;  #se aplican los privilegios
+
+
 
 
 
